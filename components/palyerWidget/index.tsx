a79e7bc08dbff68b31501455fca5d839ace5d17e
Chadibz <91723882+Chadibz@users.noreply.github.com>
2022-07-31 22:21:16 +0100

    soot maymchich

--- components/palyerWidget/index.tsx
+++ components/palyerWidget/index.tsx
@@ -3,16 +3,36 @@ import React from "react";
  import { AntDesign, FontAwesome } from "@expo/vector-icons";
 import { Song } from "../../types";
 import styles from "./styles";
+import { useEffect } from "react";
+import { Audio } from "expo-av";
+import { useState } from "react";
+
+
 
 const song={
     id:'1',
+    uri:'https://storage.googleapis.com/synchedin-storage/public/tracks/GX5MY2166332/GX5MY2166332.mp3',
     imageurl :'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/pride-album-cover-art-design-template-d2bf21df8ead7742d0af24f0143097df_screen.jpg?ts=1596467888',
-    title:'MATRABINA',
-    artist:'SAMARA'
+    title:'what they want',
+    artist:'RUSS'
 }
 
  const PlayerWidget =() =>{
-  
+    const [sound,setSound]=useState<Sound|null>(null)
+const onPlaybackStatusUpdate=(status)=>{console.log(status);}
+
+    const playCurrentSong= async()=>{
+const {sound:newSound}=await Sound.createAsync({uri:song.uri},
+    {shouldPlay:true},
+    onPlaybackStatusUpdate
+    )
+    setSound(newSound)
+    }
+
+  useEffect(()=>{
+playCurrentSong
+  },[])
+
 return (
     <View style={styles.container}>
          <Image source={{uri:song.imageurl}} style={styles.image }/>
